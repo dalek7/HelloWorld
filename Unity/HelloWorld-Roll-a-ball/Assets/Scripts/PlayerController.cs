9b6546dabf51a3be222165a0eff671f1c23a29f1
Seung <seung@Seungs-MacBook-Pro.local>
2017-08-09 18:28:10 +0900

    android

--- Unity/HelloWorld-Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Unity/HelloWorld-Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -14,9 +14,19 @@ public class PlayerController : MonoBehaviour {
 
 
 	void FixedUpdate() {
+		
+		#if UNITY_ANDROID
+		Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+		float moveHorizontal 	= -0.01f * touchDeltaPosition.x;
+		float moveVertical 		= -0.01f * touchDeltaPosition.y;
+
+		#else
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
+		#endif
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
 		rb.AddForce (movement * speed);
