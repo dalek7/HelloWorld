bd295fb2aa856589fe105116aa5fc3239e3116cf
Seung <dalek@hallym.ac.kr>
2017-12-22 22:29:55 +0900

    fixed permissions

--- HelloSound+Android/app/src/main/java/com/example/seung/soundtest/MainActivity.java
+++ HelloSound+Android/app/src/main/java/com/example/seung/soundtest/MainActivity.java
@@ -20,6 +20,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,7 +28,6 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "AudioRecordTest";
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private static String mFileName = null;
     private static String mDirSave = null;
     private RecordButton mRecordButton = null;
@@ -37,27 +37,174 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer   mPlayer = null;
 
     private TextView mTextView1 = null;
+    private TextView mTextView2 = null;
     private TextView mTextViewIdx = null;
     private String   mAndroidDeviceId = null;
     int mFileCnt = 0;
 
 
-    // Requesting permission to RECORD_AUDIO
-    private boolean permissionToRecordAccepted = false;
-    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    // Permission stuffs
+    // Changes from Android M ??
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE}; // List of permissions required
+
+
+    public void AskPermission()
+    {
+        for (String permission : PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(PERMISSIONS, PERMISSION_ALL);
+                return;
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    //Do your work.
+                } else {
+                    Toast.makeText(this, "Until you grant the permission, we cannot proceed further", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case REQUEST_RECORD_AUDIO_PERMISSION:
-                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                break;
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        AskPermission();
+
+        // Record to the external cache directory for visibility
+        //String dirsave = getExternalCacheDir().getAbsolutePath();
+        String dirname = "logger2017";
+        mDirSave = Environment.getExternalStorageDirectory() + File.separator + "data"+ File.separator + dirname;
+
+        File tDir = new File(mDirSave);
+        try {
+            tDir.mkdirs();
+        }catch (Exception e) {
+            e.printStackTrace();
+            Dbg.out("Directory not created...");
         }
-        if (!permissionToRecordAccepted ) finish();
+
+        if(tDir.exists())
+        {
+            System.out.println("DIR : "+tDir);
+        }
+
+
+        LinearLayout ll = new LinearLayout(this);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //params.height = 100;
+
+        ll.setLayoutParams(params);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+
+        mRecordButton = new RecordButton(this);
+
+        ll.addView(mRecordButton,
+                new LinearLayout.LayoutParams(
+                        450, //ViewGroup.LayoutParams.WRAP_CONTENT
+                        250,//ViewGroup.LayoutParams.WRAP_CONTENT
+                        0));
+        mPlayButton = new PlayButton(this);
+        ll.addView(mPlayButton,
+                new LinearLayout.LayoutParams(
+                        450,
+                        250,
+                        0));
+
+
+        setContentView(ll);
+
+        mAndroidDeviceId = GetUniqueID();
+        Dbg.out(mAndroidDeviceId);
+        mAndroidDeviceId = mAndroidDeviceId.substring(mAndroidDeviceId.length()-5, mAndroidDeviceId.length());
+        Dbg.out(mAndroidDeviceId);
+
+        mTextViewIdx = new TextView(this);
+        mTextViewIdx.setText(mAndroidDeviceId);
+
+        ll.addView(mTextViewIdx,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+
+        mTextView1 = new TextView(this);
+        mTextView1.setText(mDirSave);
+
+
+        ll.addView(mTextView1,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+
+        mTextView2 = new TextView(this);
+        mTextView2.setText("-");
+        ll.addView(mTextView2,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    void SetupPlay()
+    {
+        try {
+            Dbg.out("Player Setting up..."+mFileName);
+            mPlayer.setDataSource(mFileName);
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+            {
+                @Override
+                public void onCompletion(MediaPlayer mp)
+                {
+                    Log.i(LOG_TAG, "your code comes here");
+                    mPlayButton.EnablePlay();
+
+                }
+            });
+
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    // http://stackoverflow.com/a/28368570
+    public String GetUniqueID(){
+        String  myAndroidDeviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        return  myAndroidDeviceId;
 
     }
 
+
+    // AUDIO-related
     private void onRecord(boolean start) {
         if (start) {
             startRecording();
@@ -83,23 +230,44 @@ public class MainActivity extends AppCompatActivity {
         mPlayer.release();
         mPlayer = null;
     }
-    private void UpdateFileName()
+
+    private void UpdateFileName() // default option
+    {
+        UpdateFileName("3gp");
+    }
+
+    private void UpdateFileName(String ext)
     {
 
-        String str1 = mAndroidDeviceId+ "_" + ProcFile.makeFileNameWithDateTime()+String.format("_%d", mFileCnt) + ".3gp" ;
+        String str1 = mAndroidDeviceId+ "_" + ProcFile.MakeStringWithDateTimeShort()+String.format("_%d", mFileCnt) + "."+ ext ;
         mFileName = mDirSave + File.separator + str1;// "/audiorecordtest.3gp";
 
         mFileCnt++;
     }
+
+
+
     private void startRecording() {
+
         mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 
-        UpdateFileName();
+        if(false) // TODO : use GUI elements !
+        {
+            UpdateFileName("3gp");
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        }
+        else
+        {
+            UpdateFileName("mp3");
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        }
 
+        mTextView2.setText(mFileName);
         mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
 
         try {
             mRecorder.prepare();
@@ -126,13 +294,12 @@ public class MainActivity extends AppCompatActivity {
                 onRecord(mStartRecording);
                 if (mStartRecording)
                 {
-                    UpdateFileName();
-
+                    mPlayButton.setEnabled(false);
                     setText("Stop recording");
                 }
                 else
                 {
-
+                    mPlayButton.setEnabled(true);
                     setText("Start recording");
                 }
                 mStartRecording = !mStartRecording;
@@ -174,147 +341,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-    // Storage Permissions
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
 
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        // Record to the external cache directory for visibility
-        //String dirsave = getExternalCacheDir().getAbsolutePath();
-        String dirname = "logger2017";
-        mDirSave = Environment.getExternalStorageDirectory() + File.separator + "data"+ File.separator + dirname;
-
-        File tDir = new File(mDirSave);
-        try {
-            tDir.mkdirs();
-        }catch (Exception e) {
-            e.printStackTrace();
-            Dbg.out("Directory not created...");
-        }
-
-        if(tDir.exists())
-        {
-            System.out.println("DIR : "+tDir);
-        }
-
-
-        mFileName = mDirSave;
-        mFileName += "/audiorecordtest.3gp";
-
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-        verifyStoragePermissions(this);
-
-        LinearLayout ll = new LinearLayout(this);
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        //params.height = 100;
-
-        ll.setLayoutParams(params);
-        ll.setOrientation(LinearLayout.VERTICAL);
-
-
-        mRecordButton = new RecordButton(this);
-
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        150,//ViewGroup.LayoutParams.WRAP_CONTENT
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        150,
-                        0));
-
-
-
-        setContentView(ll);
-
-        mAndroidDeviceId = getUniqueID();
-        Dbg.out(mAndroidDeviceId);
-        mAndroidDeviceId = mAndroidDeviceId.substring(mAndroidDeviceId.length()-5, mAndroidDeviceId.length());
-        Dbg.out(mAndroidDeviceId);
-
-        mTextViewIdx = new TextView(this);
-        mTextViewIdx.setText(mAndroidDeviceId);
-
-        ll.addView(mTextViewIdx,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-
-        mTextView1 = new TextView(this);
-        mTextView1.setText(mDirSave);
-
-
-        ll.addView(mTextView1,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mRecorder != null) {
-            mRecorder.release();
-            mRecorder = null;
-        }
-
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
-        }
-    }
-
-    void SetupPlay()
-    {
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-            {
-                @Override
-                public void onCompletion(MediaPlayer mp)
-                {
-                    Log.i(LOG_TAG, "your code comes here");
-                    mPlayButton.EnablePlay();
-
-                }
-            });
-
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    // http://stackoverflow.com/a/28368570
-    public String getUniqueID(){
-        String  myAndroidDeviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-        return  myAndroidDeviceId;
-
-    }
 }
